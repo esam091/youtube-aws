fbaeffbc3558366d48442851b3754409865d7516
Samuel <se12345a@email.me>
2024-10-17 09:37:00 +0700

    Explicitly set cookie domain for Amplify

--- webapp/src/components/AmplifyConfig.tsx
+++ webapp/src/components/AmplifyConfig.tsx
@@ -1,18 +1,30 @@
-'use client';
+"use client";
 
 import { Amplify } from "aws-amplify";
 import { useEffect } from "react";
+import { cognitoUserPoolsTokenProvider } from "aws-amplify/auth/cognito";
+import { CookieStorage } from "aws-amplify/utils";
 
 export function AmplifyConfig() {
   useEffect(() => {
-    Amplify.configure({
-      Auth: {
-        Cognito: {
-          userPoolId: process.env.NEXT_PUBLIC_AWS_USER_POOL_ID!,
-          userPoolClientId: process.env.NEXT_PUBLIC_AWS_USER_POOL_CLIENT_ID!, 
+    Amplify.configure(
+      {
+        Auth: {
+          Cognito: {
+            userPoolId: process.env.NEXT_PUBLIC_AWS_USER_POOL_ID!,
+            userPoolClientId: process.env.NEXT_PUBLIC_AWS_USER_POOL_CLIENT_ID!,
+          },
         },
       },
-    }, {ssr: true});
+      { ssr: true }
+    );
+
+    cognitoUserPoolsTokenProvider.setKeyValueStorage(
+      new CookieStorage({
+        secure: false, // not using https in deployed beanstalk
+        domain: typeof window !== "undefined" ? window.location.hostname : "",
+      })
+    );
   }, []);
 
   return null;
